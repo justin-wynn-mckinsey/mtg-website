254b2aa66b54f048f70e7ea0fdc99db874d5986e
justinwynn719 <justinwynn719@gmail.com>
2022-10-10 02:42:56 -0400

    depoly.sh fix #2

--- public/deploy.sh
+++ public/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/maroontigergamin@github.io master:gh-pages
+git push -f git@github.com:maroontigergaming/mtg-website.git master:gh-pages
 cd -
