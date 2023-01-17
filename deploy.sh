e107f44d1a9d1a340ea01a56860e30d45e377559
justinwynn719 <justinwynn719@gmail.com>
2023-01-17 03:53:31 -0500

    trying to get back to normal

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
+
 # build
-yarn run build
+npm run build
+
 # navigate into the build output directory
 cd dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:maroontigergaming/mtg-website.git master:gh-pages
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:maroontigergaming/maroontigergaming.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:maroontigergaming/mtg-website.git main:gh-pages
+
 cd -
-chmod +x deploy.sh
